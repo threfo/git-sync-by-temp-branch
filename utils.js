5e1508e3c323a8d5e8aa8d17f51a8ad9423426d0
thomas-ballo <liurongliang@belloai.com>
2024-07-11 10:43:20 +0800

    chore: sync test to main

--- utils.js
+++ utils.js
@@ -21,7 +21,7 @@ export const gitPush = (cwd, branch = 'develop', origin = 'origin') =>
   execa('git', ['push', origin, `HEAD:${branch}`], { stdio: 'inherit', cwd })
 
 const gitLogToJson = (s) => {
-  // console.log('gitLogToJson', s)
+  console.log('gitLogToJson', s)
   const commits = s.split('commit ') // 分割每个commit
   const result = []
 
@@ -65,38 +65,39 @@ export const getGitLog = async (cwd) => {
   return list
 }
 
-const findLastSyncDate = (logs, matchMsg) => {
+const findLastSyncCommitId = (logs, matchMsg) => {
   const log = logs.find(({ message }) => message.includes(matchMsg))
-  const { date } = log || {}
-  console.log(chalk.bgGreen('findLastSyncDate date', date))
-  return date
+  const { message } = log || {}
+  const list = message.split('\n')
+  const lastSyncCommit = list.find((item) => item.startsWith('CommitId: ')) || ''
+  let lastSyncCommitId = lastSyncCommit.replace('CommitId: ', '')
+
+  console.log(chalk.bgGreen('findLastSyncDate lastSyncCommitId', lastSyncCommitId))
+  return lastSyncCommitId
 }
 
 export const getCommitMsg = async (originGitFilePath, syncGitFilePath, matchMsg) => {
   const originGitLogs = await getGitLog(originGitFilePath)
+  console.log(chalk.bgGreen('originGitLogs', JSON.stringify(originGitLogs)))
+
   const syncGitLogs = await getGitLog(syncGitFilePath)
 
-  console.log(chalk.bgGreen('originGitLogs', JSON.stringify(originGitLogs)))
   console.log(chalk.bgYellow('syncGitLogs', JSON.stringify(syncGitLogs)))
-  const lastSyncDate = findLastSyncDate(syncGitLogs, matchMsg)
+  const lastSyncCommitId = findLastSyncCommitId(syncGitLogs, matchMsg)
 
   let msg = ''
-  if (lastSyncDate) {
-    msg = originGitLogs
-      .filter(({ date, message }) => {
-        console.log(date, ': ', message)
-        console.log(
-          'date',
-          date,
-          'lastSyncDate',
-          lastSyncDate,
-          'date.getTime() > lastSyncDate.getTime()',
-          date.getTime() > lastSyncDate.getTime(),
-        )
-        return date.getTime() > lastSyncDate.getTime()
-      })
-      .map(({ message }) => message)
-      .join('\n')
+  if (lastSyncCommitId) {
+    const lastIndex = originGitLogs.findIndex(({ commitId }) => commitId === lastSyncCommitId)
+    const needCommitLogs = originGitLogs.slice(0, lastIndex)
+    const [thisTimeCommit] = needCommitLogs
+    const { commitId: thisTimeCommitId } = thisTimeCommit || {}
+
+    const msgArr = [
+      `CommitId: ${thisTimeCommitId}`,
+      ...needCommitLogs.map(({ message }) => message),
+    ]
+
+    msg = msgArr.join('\n')
     console.log(chalk.bgRed('msg', msg))
   }
 
